8b68e962eeb2442c82552a4e24c1c328da8e72cb
lethanhson9901 <hoasung9901@gmail.com>
2020-12-29 18:02:59 +0700

    show nhiet do

--- app/src/main/java/com/example/iot/MainActivity.java
+++ app/src/main/java/com/example/iot/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -29,8 +30,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.tabs.TabLayout;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -51,22 +54,28 @@ public class MainActivity extends AppCompatActivity {
     private static String USER_NAME = "sonlt173346";
     private static String PASSWORD = "sonlt173346";
     private static String TOPIC_PUB = "ESP8266/LED/status";
-    private static String TEMP_TOPIC_SUB = "ESP8266/LED/status";
-    private static String HUMID_TOPIC_SUB = "ESP8266/LED/status";
+    private static String TEMP_TOPIC_SUB = "Son1239";
+    private static String HUMID_TOPIC_SUB = "Son12349";
     private static String TOPIC_SUB = "ESP8266/LED/status";
     private static String TAG = "tag";
     private MqttAndroidClient client;
-
-
+    LineDataSet tempSet, humSet;
+    LineData lineData;
+    private TextView temperature;
+    private TextView humidity;
     private ImageView imageViewLamp1;
     private Button btnOnLamp1;
     private Button btnOffLamp1;
-
+    private LineChart lineChart;
     protected static final int RESULT_SPEECH = 1;
     private ImageButton btnSpeak;
     private TextView tvText;
     private TextToSpeech mTTS;
     public int state=0;
+    public byte[] temper;
+    public byte[] humid;
+    private float i = 0;
+    private float j = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 imageViewLamp1.setBackgroundColor(Color.GRAY);
                 String payload = "OFF";
                 publish(TOPIC_PUB, false, payload);
-                state=0;
+
             }
         });
         btnOnLamp1.setOnClickListener(new View.OnClickListener() {
@@ -92,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
                 imageViewLamp1.setBackgroundColor(Color.GREEN);
                 String payload = "ON";
                 publish(TOPIC_PUB, false, payload);
-                state=1;
+
             }
         });
 
@@ -114,6 +123,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -123,10 +135,7 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     tvText.setText(text.get(0));
                     if (text.contains("turn on the light")){
-                        if (state==1){
-                            String resText = "Opps, light is already on!";
-                            return;
-                        }
+
                         String resText = "OK, light on!";
                         mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
                             @Override
@@ -149,10 +158,11 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                         });
-                        state=1;
+
                     }
 
                     if (text.contains("turn off the light")){
+
                         String resText = "OK, light off!";
                         mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
                             @Override
@@ -174,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                         });
+                        state=0;
 
                     }
                 }
@@ -191,10 +202,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void intit() {
-
+        humidity = findViewById(R.id.textViewHum);
+        temperature = findViewById(R.id.textViewTemp);
         imageViewLamp1 = findViewById(R.id.ImgLamp1);
         btnOffLamp1 = findViewById(R.id.btnLamp1OFF);
         btnOnLamp1 = findViewById(R.id.btnLamp1ON);
+        lineChart = findViewById(R.id.lineChart);
     }
 
 
@@ -211,7 +224,8 @@ public class MainActivity extends AppCompatActivity {
                 public void onSuccess(IMqttToken asyncActionToken) {
                     // We are connected
                     Log.d(TAG, "onSuccess");
-                    subscribesToMqtt(TOPIC_SUB, 0);
+                    subscribesToMqtt();
+
                 }
 
                 @Override
@@ -226,17 +240,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void subscribesToMqtt(String topicSub, int qos) {
+    private void subscribesToMqtt() {
+        ArrayList<Entry> yTemps, yHumids;
+        yTemps = new ArrayList<>();
+        yHumids = new ArrayList<>();
         try {
-            client.subscribe(topicSub, qos);
+            client.subscribe(TOPIC_SUB, 0);
+            client.subscribe(TEMP_TOPIC_SUB, 0);
+            client.subscribe(HUMID_TOPIC_SUB, 0);
             client.setCallback(new MqttCallback() {
                 @Override
                 public void connectionLost(Throwable cause) {
+
                 }
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Log.d(TAG, topicSub + ":  " + new String(message.getPayload()));
+                    Log.d(TAG, topic + ":  " + new String(message.getPayload()));
+                    if (topic.equals(TEMP_TOPIC_SUB)){
+                        temper = message.getPayload();
+                        String s = new String(temper, StandardCharsets.UTF_8);
+                        int temper_int = Integer.parseInt(s);
+                        temperature.setText(s);
+                        yTemps.add(new Entry(i, temper_int));
+                        i += 0.25;
+                        tempSet = new LineDataSet(yTemps, "Temperature");
+                        tempSet.setColor(Color.RED);
+                        tempSet.setLineWidth(2f);
+                        tempSet.setDrawValues(false);
+                        tempSet.setCircleColor(Color.BLACK);
+                        lineData = new LineData(tempSet);
+                        lineChart.setData(lineData);
+                        lineChart.setDrawGridBackground(false);
+                        lineChart.invalidate();
+                    }
+                    else if (topic.equals(HUMID_TOPIC_SUB)){
+                        humid = message.getPayload();
+                        String s = new String(humid, StandardCharsets.UTF_8);
+                        int humid_int = Integer.parseInt(s);
+                        humidity.setText(s);
+                        yHumids.add(new Entry(j, humid_int));
+                        i += 0.25;
+                        humSet = new LineDataSet(yHumids, "Humidity");
+                        humSet.setColor(Color.BLUE);
+                        humSet.setLineWidth(2f);
+                        humSet.setDrawValues(false);
+                        humSet.setDrawValues(false);
+                        humSet.setCircleColor(Color.BLACK);
+                        lineData = new LineData(humSet);
+                        lineChart.setData(lineData);
+                        lineChart.setDrawGridBackground(false);
+                        lineChart.invalidate();
+                    }
                 }
 
                 @Override
